a8b9e0bc53b12686dc151b141a97382de0cbb001
Harry Minsky <harry.minsky@gmail.com>
2025-01-24 02:41:37 -0500

    add metadata and more stuff

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -4,6 +4,7 @@ import onehundred from "../assets/100_sketches.png";
 import runner from "../assets/runner.png";
 import pinkRunner from "../assets/pink_runner.png";
 import greenRunner from "../assets/green_runner.png";
+import twinkle from "../assets/twinkle.png";
 
 const LandingPage = () => {
   return (
@@ -19,7 +20,7 @@ const LandingPage = () => {
             duration: 0.8,
             delay: 0.5,
           }}
-          className="w-full border-t-2 border-b-2 border-green-600 overflow-hidden absolute"
+          className="w-full border-t-2 border-b-2 border-sketch-green overflow-hidden absolute"
         >
           <motion.div
             initial={{ opacity: 0 }}
@@ -29,10 +30,10 @@ const LandingPage = () => {
           >
             <div className="flex w-full whitespace-nowrap">
               <div className="marquee">
-                <h1 className="text-2xl md:text-6xl lg:text-8xl text-green-600 px-4 pt-2">
+                <h1 className="text-2xl md:text-6xl lg:text-8xl text-sketch-green px-4 pt-2">
                   March 13th 7PM at Caveat NYC •
                 </h1>
-                <h1 className="text-2xl md:text-6xl lg:text-8xl text-green-600 px-4 pt-2">
+                <h1 className="text-2xl md:text-6xl lg:text-8xl text-sketch-green px-4 pt-2">
                   March 13th 7PM at Caveat NYC •
                 </h1>
               </div>
@@ -40,7 +41,7 @@ const LandingPage = () => {
           </motion.div>
         </motion.div>
         <div className="pt-8">
-          <div className="flex pt-8 justify-center h-screen ">
+          <div className="flex pt-8 justify-center md:h-screen ">
             <div className="md:mt-[120px]">
               <motion.img
                 initial={{ opacity: 0, left: "-20vw", translateX: "0%" }}
@@ -67,7 +68,7 @@ const LandingPage = () => {
                 style={{ zIndex: 2 }}
               />
             </div>
-            <div className="relative w-[400px] h-auto flex flex-col items-center md:h-[600px] md:w-[900px] md:mt-[100px]">
+            <div className="relative w-[400px] h-auto flex flex-col items-center md:h-[400px] md:w-[600px] md:mt-[100px]">
               <motion.img
                 initial={{ opacity: 0, scale: 0 }}
                 animate={{ opacity: 1, scale: 1 }}
@@ -79,7 +80,43 @@ const LandingPage = () => {
                 src={frame}
                 style={{ zIndex: 1 }}
               />
-              <div className="absolute top-[200px] w-[90%] flex flex-col items-center md:top-[400px]">
+              <motion.div
+                initial={{
+                  opacity: 0,
+                  translateX: "0%",
+                }}
+                animate={{
+                  opacity: 1,
+                }}
+                transition={{
+                  duration: 1,
+                  ease: "easeOut",
+                  delay: 1.5
+                }}
+                className=" absolute top-0 right-[80%] max-w-[60px]"
+                style={{ zIndex: 2 }}
+              >
+                <motion.img
+                  initial={{
+                    translateX: "0%",
+                  }}
+                  animate={{
+                    rotate: [0, 180, 0, 0, 180],
+                  }}
+                  transition={{
+                    duration: 2,
+                    ease: "easeOut",
+                    times: [0, 0.8, 1.0, 1.6, 2],
+                    repeat: Infinity,
+                    repeatDelay: 1,
+                    delay: 1.5
+                  }}
+                  className="max-w-[60px]"
+                  src={twinkle}
+                  style={{ zIndex: 2 }}
+                />
+              </motion.div>
+              <div className="absolute top-[200px] w-[90%] flex flex-col items-center md:top-[300px]">
                 <motion.img
                   initial={{ opacity: 0, scale: 0 }}
                   animate={{ opacity: 1, scale: 1 }}
@@ -103,7 +140,7 @@ const LandingPage = () => {
                   className="text-center mt-4"
                   style={{ zIndex: 3 }}
                 >
-                  <h1 className="text-4xl md:text-6xl  text-green-600">
+                  <h1 className="text-4xl md:text-6xl  text-sketch-green">
                     In one hour (or less)
                   </h1>
                 </motion.div>
@@ -112,8 +149,39 @@ const LandingPage = () => {
           </div>
         </div>
       </div>
-      <div>
-          
+      <div className="mx-auto justify-center flex flex-col ">
+        <motion.div
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 1,
+            scale: { type: "spring", bounce: 0.5 },
+            delay: 2,
+          }}
+          className=""
+          style={{ zIndex: 3 }}
+        >
+          <h2 className="mt-[150px] mx-4 md:mt-0 md:mx-auto text-2xl md:text-6xl max-w-[600px] text-center  text-black ">
+            The only sketch comedy group in new york city doing 100 sketches in
+            under an hour.
+          </h2>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 1,
+            scale: { type: "spring", bounce: 0.5 },
+            delay: 2.5,
+          }}
+          className="mt-4"
+          style={{ zIndex: 3 }}
+        >
+          <h2 className=" md:mt-2 mx-4 md:mx-auto text-2xl md:text-6xl max-w-[600px] text-center  text-sketch-pink ">
+            Next Show At Caveat NYC March 13th at 7:00PM. Ticket Link coming
+            soon
+          </h2>
+        </motion.div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "sketch-pink": "#d8199d",
+        "sketch-green": "#2ba40c",
+      },
+    },
   },
   plugins: [],
 };
