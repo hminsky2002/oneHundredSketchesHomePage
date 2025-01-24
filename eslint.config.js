9f92de9ee23b0b07659209e721ce50b236609ac9
Harry Minsky <harry.minsky@gmail.com>
2025-01-23 23:58:56 -0500

    copy scaffolding from react vite project

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
+import typescriptEslint from "typescript-eslint";
+
+export default typescriptEslint.config(
+  { ignores: ["*.d.ts", "**/coverage", "**/dist"] },
+  {
+    extends: [
+      eslint.configs.recommended,
+      ...typescriptEslint.configs.recommended,
+    ],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.browser,
+      parserOptions: {
+        parser: typescriptEslint.parser,
+      },
+    },
+    rules: {
+      // your rules
+    },
+  },
+  eslintConfigPrettier
+);
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,6 @@
+
+const LandingPage = () => {
+  return <div className="homebody"></div>;
+};
+
+export { LandingPage };
